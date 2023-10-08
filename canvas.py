5e517e33255030099f959c05d5f445f3367af5eb
graham <grahamasam@gmail.com>
2023-10-07 20:41:19 -0400

    write to ppm string and tests

--- canvas.py
+++ canvas.py
@@ -23,8 +23,20 @@ class Canvas():
       return round(max(min(c * 255, 255), 0))
     
     # write image header
-    ppm = ("P3 {} {}\n255\n".format(self.width, self.height))
+    ppm = ("P3\n{} {}\n255\n".format(self.width, self.height))
 
-    
+    count = 0
+
+    for row in self.pixels:
+      count = 0
+      for color in row:
+          add = "{} {} {} ".format(to_byte(color.x), to_byte(color.y), to_byte(color.z))
+          count += len(add)
+          if count < 70:
+            ppm = ppm + add
+          else:
+            ppm = ppm + "\n" + add
+            count = len(add)
+      ppm = ppm + "\n"
 
     return ppm
--- tests_canvas.py
+++ tests_canvas.py
@@ -12,8 +12,30 @@ class testCanvas(unittest.TestCase):
     self.assertTrue(self.c.pixel_at(100,125).equals(red))
 
   def test_ppm_header(self):
-    c = Canvas(5,3)
+    c = Canvas(1,1)
     ppm = c.canvas_to_ppm_string()
+    comp = "P3\n1 1\n255\n0 0 0 \n"
+    self.assertEqual(ppm, comp)
+
+  def test_ppm_line_length(self):
+    # ppm line length should never exceed 70 characters
+    # allow certain editors to read generated files
+
+    c = Canvas(10, 2)
+    for row in c.pixels:
+      for color in row:
+        color.x = 1
+        color.y = 0.8
+        color.z = 0.6
+
+    ppm = c.canvas_to_ppm_string()
+    comp = ("""P3\n10 2\n255
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 \n""")
+    self.assertEqual(ppm, comp)
+   
 
 if __name__ == "__main__":
   unittest.main()
