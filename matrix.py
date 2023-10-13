4ab874178117fbc8d3414eb41adbf8ee3b78804f
graham <grahamasam@gmail.com>
2023-10-12 22:06:41 -0400

    matrix multiply, matrix and tuple, tests

--- matrix.py
+++ matrix.py
@@ -1,3 +1,5 @@
+from tuple import Tuple
+
 class Matrix():
   # class to represent matrices of different widths and heights
 
@@ -32,3 +34,61 @@ class Matrix():
         
     return True
   
+  # build_4_matrix takes an array of 16 numbers and places them in a new 4x4 matrix
+  # returns the created matrix
+  def build_4_matrix(array):
+    assert len(array) == 16
+    count = 0
+    new_matrix = Matrix(4,4)
+
+    for i in range(4):
+      for j in range(4):
+        new_matrix.set_index(i,j,array[count])
+        count = count + 1
+    
+    return new_matrix
+
+  def multiply_matrix(self, obj):
+    assert isinstance(obj, Matrix)
+    assert self.width == obj.height
+    
+    product_matrix = Matrix(self.height, obj.width)
+
+    for i in range(self.width):
+      sum = 0
+      for j in range(obj.height):
+        index_product = self.get_index(i, j) * obj.get_index(j, i)
+        sum += index_product
+      
+    for i in range(product_matrix.height):
+      for j in range(product_matrix.width):
+        sum = 0
+        for x in range(self.width):
+            index_product = self.get_index(i, x) * obj.get_index(x, j)
+            sum += index_product
+        product_matrix.set_index(i,j,sum)
+
+    return product_matrix
+  
+  def multiply_tuple(self, obj):
+    assert isinstance(obj, Tuple)
+    assert self.width == 4
+
+    array = [0 for i in range(4)]
+
+    for i in range(4):
+      product = self.get_index(i, 0) * obj.x + self.get_index(i, 1) * obj.y + self.get_index(i, 2) * obj.z + self.get_index(i, 3) * obj.w
+      array[i] = product
+
+    return Tuple(array[0], array[1], array[2], array[3])
+    
+  
+  # operator override to multiply matrix by matrix or matrix by tuple
+  def __mul__(self, obj):
+    # assert obj is a tuple or Matrix
+    assert isinstance(obj, (Tuple, Matrix))
+    
+    if (isinstance(obj, Tuple)):
+      return self.multiply_tuple(obj)
+    if (isinstance(obj, Matrix)):
+      return self.multiply_matrix(obj)
--- tests_matrix.py
+++ tests_matrix.py
@@ -1,11 +1,14 @@
 import unittest
 from matrix import Matrix
+from tuple import Tuple
 
 class TestMatrix(unittest.TestCase):
   def setUp(self):
     self.m = Matrix(4,4)
     self.n = Matrix(4,4)
     self.n.set_index(0,0,1)
+    self.a = Matrix.build_4_matrix((1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2))
+    self.b = Matrix.build_4_matrix((-2,1,2,3,3,2,1,-1,4,3,6,5,1,2,7,8))
   
   def test_constructor(self):
     self.assertEqual(self.m.width, 4)
@@ -31,6 +34,25 @@ class TestMatrix(unittest.TestCase):
 
   def test_equals(self):
     self.assertFalse(self.m.equals(self.n))
+
+  def test_build_4_matrix(self):
+    array = ( 1, 2, 3, 4, 2, 3, 4, 5, 3, 4, 5, 6, 4, 5, 6, 7 )
+    matrix = Matrix.build_4_matrix(array)
+    #print(matrix)
+
+  def test_matrix_multiply(self):
+     c = Matrix.build_4_matrix((20,22,50,48,44,54,114,108,40,58,110,102,16,26,46,42))
+     self.assertTrue(self.a.multiply_matrix(self.b).equals(c))
+  
+  def test_matrix_multiply(self):
+     c = Matrix.build_4_matrix((20,22,50,48,44,54,114,108,40,58,110,102,16,26,46,42))
+     self.assertTrue((self.a * self.b).equals(c))
+
+  def test_multiply_tuple(self):
+    tuple = Tuple(1,2,3,4)
+    c = Matrix.build_4_matrix((1,1,1,1,2,2,2,2,1,1,1,1,1,0,0,0))
+    self.assertTrue((c * tuple).equals(Tuple(10,20,10,1)))
+
 
 if __name__ == "__main__":
   unittest.main()
